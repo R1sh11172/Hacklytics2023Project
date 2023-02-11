97a6003ee5868906fe824fc61d3ed09263768039
R1sh11172 <85463644+R1sh11172@users.noreply.github.com>
2023-02-11 12:40:57 -0500

    commit with k-means clustering

--- stockmarket.py
+++ stockmarket.py
@@ -5,12 +5,38 @@ import datetime
 import numpy as np
 import plotly.graph_objects as go
 import warnings
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import Normalizer
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
 warnings.simplefilter
 
-companies_dict = { #dictionary for extracting company data
+companies_dict = { #dictionaryt for extracting company data
     'Amazon':'AMZN',
-    'Apple':'AAPL'
-}
+    'Apple':'AAPL',
+    'Walgreen':'WBA',
+    'Northrop Grumman':'NOC',
+    'Boeing':'BA',
+    'Lockheed Martin':'LMT',
+    'McDonalds':'MCD',
+    'Intel':'INTC',
+    'IBM':'IBM',
+    'Texas Instruments':'TXN',
+    'MasterCard':'MA',
+    'Microsoft':'MSFT',
+    'General Electrics':'GE',
+    'American Express':'AXP',
+    'Pepsi':'PEP',
+    'Coca Cola':'KO',
+    'Johnson & Johnson':'JNJ',
+    'Toyota':'TM',
+    'Honda':'HMC',
+    'Exxon':'XOM',
+    'Chevron':'CVX',
+    'Valero Energy':'VLO',
+    'Ford':'F',
+    'Bank of America':'BAC'}
 
 data_source = 'yahoo'
 start_date = '2015-04-25'
@@ -54,3 +80,68 @@ plt.ylabel('Price',fontsize = 20)
 plt.plot(df.iloc[0:30]['Open']['AMZN'],label = 'Open') # Opening prices of first 30 days are plotted against date
 plt.plot(df.iloc[0:30]['Close']['AMZN'],label = 'Close') # Closing prices of first 30 days are plotted against date
 plt.legend(loc='upper left', frameon=False,framealpha=1,prop={'size': 22}) # Properties of legend box
+
+plt.figure(figsize = (20,10)) # Adjusting figure size
+plt.title('Company:Amazon',fontsize = 20)
+plt.xticks(fontsize = 10)
+plt.yticks(fontsize = 20)
+plt.xlabel('Date',fontsize = 20)
+plt.ylabel('Price',fontsize = 20)
+plt.plot(df.iloc[0:30]['Open']['AMZN'],label = 'Open') # Opening prices of first 30 days are plotted against date
+plt.plot(df.iloc[0:30]['Close']['AMZN'],label = 'Close') # Closing prices of first 30 days are plotted against date
+plt.legend(loc='upper left', frameon=False,framealpha=1,prop={'size': 22}) # Properties of legend box
+
+plt.figure(figsize = (20,8)) 
+plt.title('Company:Amazon',fontsize = 20)
+plt.xticks(fontsize = 18)
+plt.yticks(fontsize = 20)
+plt.xlabel('Date',fontsize = 20)
+plt.ylabel('Movement',fontsize = 20)
+plt.plot(movements[0][0:30])
+
+plt.figure(figsize = (20,10)) 
+plt.title('Company:Amazon',fontsize = 20)
+plt.xticks(fontsize = 18)
+plt.yticks(fontsize = 20)
+plt.xlabel('Date',fontsize = 20)
+plt.ylabel('Volume',fontsize = 20)
+plt.plot(df['Volume']['AMZN'],label = 'Open')
+
+plt.figure(figsize = (20,8)) 
+ax1 = plt.subplot(1,2,1)
+plt.title('Company:Amazon',fontsize = 20)
+plt.xticks(fontsize = 18)
+plt.yticks(fontsize = 20)
+plt.xlabel('Date',fontsize = 20)
+plt.ylabel('Movement',fontsize = 20)
+plt.plot(movements[0]) 
+plt.subplot(1,2,2,sharey = ax1)
+plt.title('Company:Apple',fontsize = 20)
+plt.xticks(fontsize = 18)
+plt.yticks(fontsize = 20)
+plt.xlabel('Date',fontsize = 20)
+plt.ylabel('Movement',fontsize = 20)
+plt.plot(movements[1])
+
+normalizer = Normalizer()
+norm_movements = normalizer.fit_transform(movements)
+print(norm_movements.min())
+print(norm_movements.max())
+print(norm_movements.mean())
+
+
+normalizer = Normalizer()
+kmeans = KMeans(n_clusters = 10,max_iter = 1000)
+pipeline = make_pipeline(normalizer,kmeans)
+pipeline.fit(movements)
+predictions = pipeline.predict(movements)
+df1 = pd.DataFrame({'labels':labels,'companies':list(companies_dict)}).sort_values(by=['labels'],axis = 0)
+
+
+normalizer = Normalizer()
+reduced_data = PCA(n_components = 2)
+kmeans = KMeans(n_clusters = 10,max_iter = 1000)
+pipeline = make_pipeline(normalizer,reduced_data,kmeans)
+pipeline.fit(movements)
+predictions = pipeline.predict(movements)
+df2 = pd.DataFrame({'labels':labels,'companies':list(companies_dict.keys())}).sort_values(by=['labels'],axis = 0)
