241dce577301a4309e9095c75155daef0282a586
Shyamsaibethina <shyamsaibethina@gmail.com>
2023-02-12 00:06:28 -0500

    -tried added twilio with js

--- hack2023/src/send_sms.js
+++ hack2023/src/send_sms.js
@@ -2,13 +2,11 @@
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
 
-import { twilio } from "twilio";
-
+const client = require("twilio");
 export function send_sms() {
   const accountSid = "AC4145f33db19c8adff71fab19c45859e1";
   const authToken = "3c9810f77e383ed12cb1d14a96898c34";
-  const client = require("twilio")(accountSid, authToken);
-
+  client(accountSid, authToken);
   client.messages
     .create({
       body: "Hello",
