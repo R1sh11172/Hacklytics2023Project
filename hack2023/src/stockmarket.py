73247a012f396d9bbf8f5ace861b1b820b0b2278
Vivek Chittepu <44039386+vivekranadive@users.noreply.github.com>
2023-02-12 05:53:27 -0500

    final changes ready for submission

--- hack2023/src/stockmarket.py
+++ hack2023/src/stockmarket.py
@@ -16,6 +16,8 @@ from twilio.rest import Client
 from pandas.plotting import table
 from IPython.display import display
 import dataframe_image as dfi
+import streamlit as st
+from sklearn.decomposition import PCA
 
 
 warnings.simplefilter
@@ -44,10 +46,11 @@ companies_dict = { #dictionaryt for extracting company datacd
     'Chevron':'CVX',
     'Valero Energy':'VLO',
     'Ford':'F',
+    'Tesla':'TSLA',
     'Bank of America':'BAC'}
 
-start_date = '2015-04-25'
-end_date = '2020-04-25'
+start_date = '2017-02-12'
+end_date = '2023-02-12'
 df = pdr.get_data_yahoo(list(companies_dict.values()), start_date, end_date)
 
 df.head()
@@ -55,86 +58,86 @@ df.isna().sum()
 
 stock_open = np.array(df['Open']).T
 stock_close = np.array(df['Close']).T
-movements = stock_close-stock_open # positive moment implies you can buy the stock
+movements = stock_close-stock_open
 sum_of_movement = np.sum(movements,1)
 for i in range(len(companies_dict)):
     print('company:{}, Change:{}'.format(df['High'].columns[i],sum_of_movement))
 
-plt.figure(figsize = (20,10)) 
-plt.subplot(1,2,1) 
+# plt.figure(figsize = (20,10)) 
+# plt.subplot(1,2,1) 
 
-plt.title('Company:Amazon',fontsize = 20)
-plt.xticks(fontsize = 10)
-plt.yticks(fontsize = 20)
-plt.xlabel('Date',fontsize = 15)
-plt.ylabel('Opening price',fontsize = 15)
-plt.plot(df['Open']['AMZN'])
-plt.savefig('hack2023/src/my_plot5.png')
-plt.subplot(1,2,2) 
+# plt.title('Company:Amazon',fontsize = 20)
+# plt.xticks(fontsize = 10)
+# plt.yticks(fontsize = 20)
+# plt.xlabel('Date',fontsize = 15)
+# plt.ylabel('Opening price',fontsize = 15)
+# plt.plot(df['Open']['AMZN'])
+# plt.savefig('hack2023/src/my_plot5.png')
+# plt.subplot(1,2,2) 
 
-plt.title('Company:Apple',fontsize = 20)
-plt.xticks(fontsize = 10)
-plt.yticks(fontsize = 20)
-plt.xlabel('Date',fontsize = 15)
-plt.ylabel('Opening price',fontsize = 15)
-plt.plot(df['Open']['AAPL'])
-plt.savefig('hack2023/src/my_plot3.png')
+# plt.title('Company:Apple',fontsize = 20)
+# plt.xticks(fontsize = 10)
+# plt.yticks(fontsize = 20)
+# plt.xlabel('Date',fontsize = 15)
+# plt.ylabel('Opening price',fontsize = 15)
+# plt.plot(df['Open']['AAPL'])
+# plt.savefig('hack2023/src/my_plot3.png')
 
 
-plt.figure(figsize = (20,10)) # Adjusting figure size
-plt.title('Company:Amazon',fontsize = 20)
-plt.xticks(fontsize = 10)
-plt.yticks(fontsize = 20)
-plt.xlabel('Date',fontsize = 20)
-plt.ylabel('Price',fontsize = 20)
-plt.plot(df.iloc[0:30]['Open']['AMZN'],label = 'Open') # Opening prices of first 30 days are plotted against date
-plt.plot(df.iloc[0:30]['Close']['AMZN'],label = 'Close') # Closing prices of first 30 days are plotted against date
-plt.legend(loc='upper left', frameon=False,framealpha=1,prop={'size': 22}) # Properties of legend box
+# plt.figure(figsize = (20,10))
+# plt.title('Company:Amazon',fontsize = 20)
+# plt.xticks(fontsize = 10)
+# plt.yticks(fontsize = 20)
+# plt.xlabel('Date',fontsize = 20)
+# plt.ylabel('Price',fontsize = 20)
+# plt.plot(df.iloc[0:30]['Open']['AMZN'],label = 'Open')
+# plt.plot(df.iloc[0:30]['Close']['AMZN'],label = 'Close') 
+# plt.legend(loc='upper left', frameon=False,framealpha=1,prop={'size': 22}) 
 
-plt.figure(figsize = (20,10)) # Adjusting figure size
-plt.title('Company:Amazon',fontsize = 20)
-plt.xticks(fontsize = 10)
-plt.yticks(fontsize = 20)
-plt.xlabel('Date',fontsize = 20)
-plt.ylabel('Price',fontsize = 20)
-plt.plot(df.iloc[0:30]['Open']['AMZN'],label = 'Open') # Opening prices of first 30 days are plotted against date
-plt.plot(df.iloc[0:30]['Close']['AMZN'],label = 'Close') # Closing prices of first 30 days are plotted against date
-plt.legend(loc='upper left', frameon=False,framealpha=1,prop={'size': 22}) # Properties of legend box
+# plt.figure(figsize = (20,10))
+# plt.title('Company:Amazon',fontsize = 20)
+# plt.xticks(fontsize = 10)
+# plt.yticks(fontsize = 20)
+# plt.xlabel('Date',fontsize = 20)
+# plt.ylabel('Price',fontsize = 20)
+# plt.plot(df.iloc[0:30]['Open']['AMZN'],label = 'Open')
+# plt.plot(df.iloc[0:30]['Close']['AMZN'],label = 'Close')
+# plt.legend(loc='upper left', frameon=False,framealpha=1,prop={'size': 22})
 
-plt.figure(figsize = (20,8)) 
-plt.title('Company:Amazon',fontsize = 20)
-plt.xticks(fontsize = 18)
-plt.yticks(fontsize = 20)
-plt.xlabel('Date',fontsize = 20)
-plt.ylabel('Movement',fontsize = 20)
-plt.plot(movements[0][0:30])
-plt.savefig('hack2023/src/my_plot6.png')
+# plt.figure(figsize = (20,8)) 
+# plt.title('Company:Amazon',fontsize = 20)
+# plt.xticks(fontsize = 18)
+# plt.yticks(fontsize = 20)
+# plt.xlabel('Date',fontsize = 20)
+# plt.ylabel('Movement',fontsize = 20)
+# plt.plot(movements[0][0:30])
+# plt.savefig('hack2023/src/my_plot6.png')
 
-plt.figure(figsize = (20,10)) 
-plt.title('Company:Amazon',fontsize = 20)
-plt.xticks(fontsize = 18)
-plt.yticks(fontsize = 20)
-plt.xlabel('Date',fontsize = 20)
-plt.ylabel('Volume',fontsize = 20)
-plt.plot(df['Volume']['AMZN'],label = 'Open')
-plt.savefig('hack2023/src/my_plot4.png')
+# plt.figure(figsize = (20,10)) 
+# plt.title('Company:Amazon',fontsize = 20)
+# plt.xticks(fontsize = 18)
+# plt.yticks(fontsize = 20)
+# plt.xlabel('Date',fontsize = 20)
+# plt.ylabel('Volume',fontsize = 20)
+# plt.plot(df['Volume']['AMZN'],label = 'Open')
+# plt.savefig('hack2023/src/my_plot4.png')
 
 plt.figure(figsize = (20,8)) 
 ax1 = plt.subplot(1,2,1)
-plt.title('Company:Amazon',fontsize = 20)
+plt.title('Company:Tesla',fontsize = 20)
 plt.xticks(fontsize = 18)
 plt.yticks(fontsize = 20)
 plt.xlabel('Date',fontsize = 20)
 plt.ylabel('Movement',fontsize = 20)
 plt.plot(movements[0]) 
 plt.subplot(1,2,2,sharey = ax1)
-plt.title('Company:Apple',fontsize = 20)
+plt.title('Company:Microsoft',fontsize = 20)
 plt.xticks(fontsize = 18)
 plt.yticks(fontsize = 20)
 plt.xlabel('Date',fontsize = 20)
 plt.ylabel('Movement',fontsize = 20)
 plt.plot(movements[1])
-plt.savefig('hack2023/src/my_plot2.png')
+plt.savefig('hack2023/src/my_plot3.png')
 
 
 normalizer = Normalizer()
@@ -150,7 +153,7 @@ pipeline = make_pipeline(normalizer,kmeans)
 pipeline.fit(movements)
 predictions = pipeline.predict(movements)
 df1 = pd.DataFrame({'Cluster':predictions,'companies':list(companies_dict)}).sort_values(by=['Cluster'],axis = 0)
-dfi.export(df1, 'dataframe1.png')
+#dfi.export(df1, 'hack2023/src/dataframe1.png')
 
 
 normalizer = Normalizer()
@@ -160,50 +163,25 @@ pipeline = make_pipeline(normalizer,reduced_data,kmeans)
 pipeline.fit(movements)
 predictions = pipeline.predict(movements)
 df2 = pd.DataFrame({'Cluster':predictions,'companies':list(companies_dict.keys())}).sort_values(by=['Cluster'],axis = 0)
-dfi.export(df1, 'dataframe2.png')
+dfi.export(df2, 'hack2023/src/my_plot1.png')
 
-from sklearn.decomposition import PCA
-# Reduce the data
 reduced_data = PCA(n_components = 2).fit_transform(norm_movements)
-# Define step size of mesh
 h = 0.01
-# Plot the decision boundary
 x_min,x_max = reduced_data[:,0].min()-1, reduced_data[:,0].max() + 1
 y_min,y_max = reduced_data[:,1].min()-1, reduced_data[:,1].max() + 1
 xx,yy = np.meshgrid(np.arange(x_min,x_max,h),np.arange(y_min,y_max,h))
-# Obtain labels for each point in the mesh using our trained model
 Z = kmeans.predict(np.c_[xx.ravel(),yy.ravel()])
-# Put the result into a color plot
 Z = Z.reshape(xx.shape)
-# Define color plot
 cmap = plt.cm.Paired
-# Plotting figure
 plt.clf()
 plt.figure(figsize=(10,10))
 plt.imshow(Z,interpolation = 'nearest',extent=(xx.min(),xx.max(),yy.min(),yy.max()),cmap = cmap,aspect = 'auto',origin = 'lower')
 plt.plot(reduced_data[:,0],reduced_data[:,1],'k.',markersize = 5)
 
-# Plot the centroid of each cluster as a white X
+
 centroids = kmeans.cluster_centers_
 plt.scatter(centroids[:,0],centroids[:,1],marker = 'x',s = 169,linewidths = 3,color = 'w',zorder = 10)
 plt.title('K-Means clustering on stock market movements (PCA-Reduced data)')
 plt.xlim(x_min,x_max)
 plt.ylim(y_min,y_max)
-plt.show()
-plt.savefig('hack2023/src/my_plot1.png')
-
-def twilio(input):
-    account_sid = 'AC4145f33db19c8adff71fab19c45859e1'
-    auth_token = 'bd8f9acaf45717159680d027d982a3c2'
-    client = Client(account_sid, auth_token)
-
-    message = client.messages \
-        .create(
-            body='The wesbite has been updated with all the new predictions',
-            from_='+14694614640',
-            to=input
-        )
-
-    print(message.sid)
-    return
-twilio('+14695697885')
+plt.savefig('hack2023/src/my_plot2.png')
